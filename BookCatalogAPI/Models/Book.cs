75fd53bbe1af4431fde7e637392d5b23d6996e16
Aqeish Parvez <aqeish.parvez@gmail.com>
2023-11-22 01:19:07 -0500

    Basic book api setup

---
+++ BookCatalogAPI/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookCatalogAPI.Models
+{
+    public class Book
+    {
+        [Key]
+        public int BookId { get; set; }
+
+        [ Required]
+        public string Title { get; set; } = "Unknown";
+        public string Isbn { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+
+    }
+}
